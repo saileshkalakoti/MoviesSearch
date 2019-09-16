551708157d4c34cbc8dc6376b7d75c80295a8360
kalakotisaileshh <saileshkalakoti>
2019-09-16 15:54:46 +0530

    Updates

---
+++ precache-manifest.ebd35d8cacdef5b77d621922c84188d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a344b5d0a4b2e0a5e3a5a44d343269",
+    "url": "/MoviesSearch/index.html"
+  },
+  {
+    "revision": "30ef51ab375ccc5e0d26",
+    "url": "/MoviesSearch/static/css/main.777ef555.chunk.css"
+  },
+  {
+    "revision": "dc6f74c7cec37690c107",
+    "url": "/MoviesSearch/static/js/2.dc1c9c96.chunk.js"
+  },
+  {
+    "revision": "30ef51ab375ccc5e0d26",
+    "url": "/MoviesSearch/static/js/main.07a15c40.chunk.js"
+  },
+  {
+    "revision": "1fa6ca12291aa9b5cde1",
+    "url": "/MoviesSearch/static/js/runtime~main.972018ef.js"
+  },
+  {
+    "revision": "a567c2208beec98f3d27a442304a3033",
+    "url": "/MoviesSearch/static/media/icon.a567c220.svg"
+  }
+]);
